1be5121dbdb6711097023eeb6d2a713a96578e13
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-08-30 20:06:29 +0700

    test for default params changed. test for assigned params added

--- goods/tests.py
+++ goods/tests.py
@@ -5,6 +5,19 @@ from .models import Product
 class ProductModelTest(TestCase):
     def test_product_can_be_created_with_default_params(self):
         Product.objects.create(
+            title='product one'
+        )
+
+        product = Product.objects.first()
+        self.assertRegexpMatches(
+            str(product.id),
+            r'\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b')
+        self.assertEqual(product.title, 'product one')
+        self.assertEqual(product.description, '')
+        self.assertEqual(float(product.price), 0.00)
+
+    def test_product_can_be_created_with_assigned_params(self):
+        Product.objects.create(
             title='product one',
             description='product one description',
             price=1.15,
