aa8105e603977fa2b755dc73acaec5ef2c46882c
Marat Komaltilov <marat.komaltilov@icloud.com>
2020-08-30 19:42:13 +0700

    base product model added

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=500, default='')
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
--- goods/tests.py
+++ goods/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductModelTest(TestCase):
+    def test_product_can_be_created_with_default_params(self):
+        Product.objects.create(
+            title='product one',
+            description='product one description',
+            price=1.15,
+        )
+
+        product = Product.objects.first()
+        self.assertRegexpMatches(
+            str(product.id),
+            r'\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b')
+        self.assertEqual(product.title, 'product one')
+        self.assertEqual(product.description, 'product one description')
+        self.assertEqual(float(product.price), 1.15)
